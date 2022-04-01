16fcb70bb2fc61aa9904f64db4324ef211f40002
Seth Richard <sric111@lsu.edu>
2022-04-01 14:09:55 -0500

    - Added binary to float conversion

--- HonorsProject/script.js
+++ HonorsProject/script.js
@@ -6,8 +6,13 @@ const NUM_SIGN_BITS = 1;
 function RunConverters() {
     var input = $("#floatInput").val();
     var binOutput = toBinary(input);
+    var floatFromBinary = toFloat(binOutput);
+    var errorPercentage = (1 - floatFromBinary / input) * 100;
 
-    $('#outputSpan').text(binOutput);
+    $('#originalNumber').text(input);
+    $('#binaryOutput').text(binOutput);
+    $('#floatFromBinary').text(floatFromBinary);
+    $('#errorPercentage').text(errorPercentage + "%");
 }
 
 function toBinary(number) {
@@ -18,13 +23,12 @@ function toBinary(number) {
     
     // Check if the number is negative
     var negativeBit = 0;
-    if (wholeNumber < 0) {
+    if (number < 0) {
         wholeNumber *= -1;
         negativeBit = 1;
     }
     
     // Convert to binary
-    console.log(wholeNumber + "." + decimal);
     var binaryInt = intToBinary(wholeNumber);
     var binaryFloat = decimalToBinary(decimal); 
     
@@ -35,38 +39,9 @@ function toBinary(number) {
     return negativeBit + " | " + exponentAndMantissa;
 }
 
-// function ToBinary(input) {
-
-//     var number = parseFloat(input);
-
-//     var negativeBit = "0";
-//     var exponentAndMantissa = "";
-//     if (number < 0) {
-//         number *= -1;
-//         negativeBit = "1";
-//     }
-
-//     var wholeNumber = Math.floor(number);
-//     var decimal = number - wholeNumber;
-//     console.log(wholeNumber);
-//     console.log(decimal);
-
-//     if (wholeNumber != 0) {
-//         var wholeNumStr = intToBinary(wholeNumber);
-//         var decimalStr = decimalToBinary(decimal);
-//         exponentAndMantissa = findMantissaAndExponent(wholeNumStr, decimalStr);
-//     } else {
-//         var decimalStr = decimalToBinary(decimal);
-//         exponentAndMantissa = findMantissaAndExponent("0", decimalStr);
-//     }
-
-//     return negativeBit + " | " + exponentAndMantissa;
-
-// }
-
 function intToBinary(num) {
     var result = "";
-    if (num == 0) {
+    if (num === 0) {
         result = result + "0"; //sb.append("0");
     } else {
         // Save remainder when dividing by two and half the number
@@ -142,4 +117,59 @@ function findMantissaAndExponent(num, dec) {
     }
 
     return exponent + " | " + mantissa;
+}
+
+function toFloat(binary) {
+    
+    var split = binary.split(" | ");
+
+    var exponent = binaryToInt(split[1]) - BIAS;
+
+    var wholeNumber = 0;
+    var decimal = 0.0;
+    if (exponent >= 0) {
+        wholeNumber = binaryToInt("1" + split[2].substring(0, exponent));
+        decimal = binaryToFloat(split[2].substring(exponent));
+    } else {
+        wholeNumber = 0;
+        var temp = split[2];
+        temp = "1" + temp;
+        for (let i = exponent + 1; i < 0; i++) {
+            temp = "0" + temp;
+        }
+
+        decimal = binaryToFloat(temp);
+    }
+
+    var value = wholeNumber + decimal;
+
+    if (split[0] === "1") {
+        value = value * -1;
+    }
+    
+    //var errorPercentage = (1 - value / originalDecs[counter]) * 100; 
+    
+    //var outputString = ", Converted Value: " + value + ", Error Percentage: " + errorPercentage + "%";
+    
+    return value;
+}
+
+function binaryToInt(bin) {
+    var value = 0;
+    for (let i = bin.length - 1; i >= 0; i--) {
+        // Each digit in a binary value has a value of {DIGIT} * 2^(i) where i is the distance from the right most digit (so the right-most digit has an i value of 0)
+        value += parseInt(bin.charAt(i) + "") * (Math.pow(2, (bin.length - i - 1)));
+    }
+
+    return value;
+}
+
+function binaryToFloat(bin) {
+    var value = 0.0;
+    for (let i = 0; i < bin.length - 1; i++) {
+        // Each digit after the decimal point is 1 / 2^(i) where i = the distance from the decimal point (so the first digit after the . has an i value of 0), when counting left to right. 
+        value += parseInt(bin.charAt(i) + "") * 1 / (Math.pow(2, i + 1));
+    }
+
+    return value;
 }
