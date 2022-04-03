cc985d4feeea95bab3e9c991e98b4d981f20d434
Seth Holden Richard <holdenbrown10@gmail.com>
2022-04-03 16:05:54 -0500

    - Added double percision

--- HonorsProject/script.js
+++ HonorsProject/script.js
@@ -1,21 +1,41 @@
 const BIAS = 127;
-const NUM_EXPONENT_BITS = 8;
-const NUM_FRACTION_BITS = 23;
+const NUM_EXPONENT_BITS_SINGLE = 8;
+const NUM_EXPONENT_BITS_DOUBLE = 11;
+const NUM_FRACTION_BITS_SINGLE = 23;
+const NUM_FRACTION_BITS_DOUBLE = 52;
 const NUM_SIGN_BITS = 1;
 
 function RunConverters() {
     var input = $("#floatInput").val();
-    var binOutput = toBinary(input);
+
+    SinglePercision(input);
+    DoublePercision(input);
+}
+
+function SinglePercision(input) {
+    var binOutput = toBinary(input, false);
     var floatFromBinary = toFloat(binOutput);
     var errorPercentage = (1 - floatFromBinary / input) * 100;
 
-    $('#originalNumber').text(input);
-    $('#binaryOutput').text(binOutput);
-    $('#floatFromBinary').text(floatFromBinary);
-    $('#errorPercentage').text(errorPercentage + "%");
+    $('#originalNumberSingle').text(input);
+    $('#binaryOutputSingle').text(binOutput);
+    $('#floatFromBinarySingle').text(floatFromBinary);
+    $('#errorPercentageSingle').text(errorPercentage + "%");
 }
 
-function toBinary(number) {
+function DoublePercision(input) {
+    var binOutput = toBinary(input, true);
+    var floatFromBinary = toFloat(binOutput);
+    var errorPercentage = (1 - floatFromBinary / input) * 100;
+
+    $('#originalNumberDouble').text(input);
+    $('#binaryOutputDouble').text(binOutput);
+    $('#floatFromBinaryDouble').text(floatFromBinary);
+    $('#errorPercentageDouble').text(errorPercentage + "%");
+}
+
+
+function toBinary(number, doubleFlag) {
     // Split the input string into the whole number part and the decimal part
     var temp = number.split(".");
     var decimal = parseFloat("." + temp[1]);
@@ -33,50 +53,18 @@ function toBinary(number) {
     var binaryFloat = decimalToBinary(decimal); 
     
     // Combine whole number and decimal and find exponent
-    var exponentAndMantissa = findMantissaAndExponent(binaryInt, binaryFloat);
-    
+    var exponentAndMantissa = "";
+    if (doubleFlag === true) {
+        exponentAndMantissa = findMantissaAndExponentDouble(binaryInt, binaryFloat);
+    }
+    else {
+        exponentAndMantissa = findMantissaAndExponentSingle(binaryInt, binaryFloat);
+    }
     
     return negativeBit + " | " + exponentAndMantissa;
 }
 
-function intToBinary(num) {
-    var result = "";
-    if (num === 0) {
-        result = result + "0"; //sb.append("0");
-    } else {
-        // Save remainder when dividing by two and half the number
-        while (num != 0) {
-            result = num % 2 + result;
-            //sb.insert(0, num % 2);
-            num = Math.floor(num / 2);
-        }
-    }
-
-    return result; //sb.toString();
-}
-
-function decimalToBinary(num) {
-    // Multiply by two and save the first number, then subtract by one if that value was a 1.
-    result = ""; //StringBuilder sb = new StringBuilder();
-    while (num != 0 && num > 0) {
-        num = num * 2;
-        if (num >= 1) {
-            result = result + "1"; //sb.append(1);
-            //var one = 1;
-            num = num - 1;
-        } else {
-            result = result + "0"; //sb.append(0);
-        }
-    }
-
-    result = result + "0";
-
-    //sb.append(0);
-    
-    return result //sb.toString();
-}
-
-function findMantissaAndExponent(num, dec) {
+function findMantissaAndExponentSingle(num, dec) {
     // Find the first 1 in the whole number part, then exp = BIAS + the number of times shifted 
     var shiftIndex = num.indexOf('1');
     var exponentInt = 0;
@@ -92,24 +80,24 @@ function findMantissaAndExponent(num, dec) {
 
     var exponent = intToBinary(exponentInt);
 
-    if (mantissa.length > NUM_FRACTION_BITS) {
+    if (mantissa.length > NUM_FRACTION_BITS_SINGLE) {
         // Cut off end if the number is too long
-        mantissa = mantissa.substring(0, NUM_FRACTION_BITS);
-    } else if (mantissa.length < NUM_FRACTION_BITS) {
+        mantissa = mantissa.substring(0, NUM_FRACTION_BITS_SINGLE);
+    } else if (mantissa.length < NUM_FRACTION_BITS_SINGLE) {
         // Append zeros to the end if it is not long enough
-        var count = NUM_FRACTION_BITS - mantissa.length;
+        var count = NUM_FRACTION_BITS_SINGLE - mantissa.length;
         while (count != 0) {
             mantissa += "0";
             count--;
         }
     }
 
-    if (exponent.length > NUM_EXPONENT_BITS) {
+    if (exponent.length > NUM_EXPONENT_BITS_SINGLE) {
         // Cut off end if the number is too long, though this should never realistically happen and if it did an overflow should occur.
-        exponent = exponent.substring(0, NUM_EXPONENT_BITS - 1);
-    } else if (exponent.length < NUM_EXPONENT_BITS) {
+        exponent = exponent.substring(0, NUM_EXPONENT_BITS_SINGLE - 1);
+    } else if (exponent.length < NUM_EXPONENT_BITS_SINGLE) {
         // Pad with zeros if it is too small
-        var count = NUM_EXPONENT_BITS - exponent.length;
+        var count = NUM_EXPONENT_BITS_SINGLE - exponent.length;
         while (count != 0) {
             exponent = "0" + exponent;
             count--;
@@ -117,6 +105,83 @@ function findMantissaAndExponent(num, dec) {
     }
 
     return exponent + " | " + mantissa;
+}
+
+function findMantissaAndExponentDouble(num, dec) {
+    // Find the first 1 in the whole number part, then exp = BIAS + the number of times shifted 
+    var shiftIndex = num.indexOf('1');
+    var exponentInt = 0;
+    var mantissa = "";
+    if (shiftIndex != -1) {
+        exponentInt = BIAS + (num.length - shiftIndex - 1);
+        mantissa = num.substring(shiftIndex + 1) + dec;
+    } else {
+        shiftIndex = dec.indexOf('1') + 1;
+        exponentInt = BIAS - (shiftIndex);
+        mantissa = dec.substring(shiftIndex);
+    }
+
+    var exponent = intToBinary(exponentInt);
+
+    if (mantissa.length > NUM_FRACTION_BITS_DOUBLE) {
+        // Cut off end if the number is too long
+        mantissa = mantissa.substring(0, NUM_FRACTION_BITS_DOUBLE);
+    } else if (mantissa.length < NUM_FRACTION_BITS_DOUBLE) {
+        // Append zeros to the end if it is not long enough
+        var count = NUM_FRACTION_BITS_DOUBLE - mantissa.length;
+        while (count != 0) {
+            mantissa += "0";
+            count--;
+        }
+    }
+
+    if (exponent.length > NUM_EXPONENT_BITS_DOUBLE) {
+        // Cut off end if the number is too long, though this should never realistically happen and if it did an overflow should occur.
+        exponent = exponent.substring(0, NUM_EXPONENT_BITS_DOUBLE - 1);
+    } else if (exponent.length < NUM_EXPONENT_BITS_DOUBLE) {
+        // Pad with zeros if it is too small
+        var count = NUM_EXPONENT_BITS_DOUBLE - exponent.length;
+        while (count != 0) {
+            exponent = "0" + exponent;
+            count--;
+        }
+    }
+
+    return exponent + " | " + mantissa;
+}
+
+function intToBinary(num) {
+    var result = "";
+    if (num === 0) {
+        result = result + "0"; 
+    } else {
+        // Save remainder when dividing by two and half the number
+        while (num != 0) {
+            result = num % 2 + result;
+            num = Math.floor(num / 2);
+        }
+    }
+
+    return result; 
+}
+
+function decimalToBinary(num) {
+    // Multiply by two and save the first number, then subtract by one if that value was a 1.
+    result = ""; 
+    while (num != 0 && num > 0) {
+        num = num * 2;
+        if (num >= 1) {
+            result = result + "1"; 
+            //var one = 1;
+            num = num - 1;
+        } else {
+            result = result + "0"; 
+        }
+    }
+
+    result = result + "0";
+    
+    return result 
 }
 
 function toFloat(binary) {
@@ -146,10 +211,6 @@ function toFloat(binary) {
     if (split[0] === "1") {
         value = value * -1;
     }
-    
-    //var errorPercentage = (1 - value / originalDecs[counter]) * 100; 
-    
-    //var outputString = ", Converted Value: " + value + ", Error Percentage: " + errorPercentage + "%";
     
     return value;
 }
